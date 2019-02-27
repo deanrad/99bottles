e64f26839d808dfa80ea6e469fb44e49ed5b841c
Dean Radcliffe <dradcliffe@g2crowd.com>
2019-02-27 08:31:10 -0600

    First commit

---
+++ bottles.js
@@ -0,0 +1,48 @@
+const Singer = require("rx-helper").agent;
+const { range } = require("rxjs");
+const { map } = require("rxjs/operators");
+
+const MAX = 9;
+// Singer.addFilter(({ action }) =>
+//   console.log(`${action.type}: ${action.payload}`)
+// );
+
+Singer.on(
+  "singIt",
+  () => {
+    return range(0, MAX + 1).pipe(map(i => MAX - i));
+  },
+  {
+    type: "verse"
+  }
+);
+
+Singer.on("verse", ({ action }) => {
+  const number = action.payload;
+  console.log(`${pluralOf(number, true)} of beer on the wall, ${pluralOf(
+    number
+  )} of beer.
+${ending(number)}
+`);
+});
+
+function pluralOf(count, capital = false, noun = "bottle") {
+  if (count >= 2) {
+    return `${count} ${noun}s`;
+  } else if (count == 1) {
+    return `${count} ${noun}`;
+  } else {
+    return (capital ? "No" : "no") + ` more ${noun}s`;
+  }
+}
+
+function ending(number) {
+  const article = number == 1 ? "it" : "one";
+  return number == 0
+    ? "Go to the store and buy some more, 99 bottles of beer on the wall."
+    : `Take ${article} down and pass it around, ${pluralOf(
+        number - 1
+      )} of beer on the wall.`;
+}
+
+Singer.process({ type: "singIt" });
